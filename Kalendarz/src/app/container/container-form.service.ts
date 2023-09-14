a329e40fff8b93034a6aae7e950c8e8277979f70
Pawliks <pawliks.ph@gmail.com>
2023-09-14 22:03:51 +0200

    add form service

---
+++ Kalendarz/src/app/container/container-form.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ContainerFormService {
+    url = "https://kalendarz-zsen.web.app/kalendarz"
+
+    constructor(private http: HttpClient) {}
+
+    events() {
+        return this.http.get(this.url)
+    }
+    saveEvent(data: any) {
+        const headers = new HttpHeaders({
+            'Authorization': 'dupa1234', 
+          });
+        return this.http.post(this.url,{data},{ headers })
+    }
+}
