1de91ddef5f0f3c7f4508ba4436b61de871c4bdd
dawidoczek <rej.dawido@gmail.com>
2023-09-15 18:25:55 +0200

    fixed me being a dumbo xoxo

--- Kalendarz/src/app/container/container-form.service.ts
+++ Kalendarz/src/app/container/container-form.service.ts
@@ -26,11 +26,22 @@ export class ContainerFormService {
           const result = this.http.post(this.url,data,{ headers });
         // magiczna sztuczka polegająca na po prostu odczytaniu rzeczy z bazy XDDD
         // czego to człowiek nie zrobi zeby miec FIREBASE REALTIMEDATABASE 
-        // narazie nie działa ofc
-        //   this.http.get<any>('https://kalendarz-zsen.web.app/baza',  { headers } ).subscribe(data => {
-        //     // this.text1 = data.message;
-        //     this.sharedDataService.updateWydarzenia(data)
-        // });
+        // ^ btw tą sztuczkę trzeba robić za kazdym razem jak odswiezamy baze. meh
+        // narazie nie działa ofc   
+        // juz działa 30 sekund później, zapomnialem sparsować danych
+          this.http.get<any>('https://kalendarz-zsen.web.app/baza',  { headers } ).subscribe(data => {
+            // this.text1 = data.message;
+            let wydarzenia;
+            wydarzenia = Object.keys(data["wydarzenia"]).map(key => data["wydarzenia"][key]); // pierdolone obiekty i ich losowe nazwy
+            
+            
+            wydarzenia.sort((a: { data: string; }, b: { data: string; }) => {
+            const dateA = a.data.split("-").join("-");
+            const dateB = b.data.split("-").join("-");
+            return dateA.localeCompare(dateB);
+            });
+     this.sharedDataService.updateWydarzenia(wydarzenia);
+        });
         
         return result
     }
